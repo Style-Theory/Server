aefff843da096887c5ff412e585366f06c637f8d
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-06 09:16:45 +0700

    add props in json

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,10 +11,10 @@ class UserController {
             name
         })
         .then((result) => {
-            let {id,email} = result.dataValues
+            let {id} = result.dataValues
             let token = Helper.getToken({id,email})
             req.headers.token = token
-            res.status(200).json({id,email,token})
+            res.status(200).json({id,email,name,token})
         }).catch((err) => {
             res.status(500).json(err)
         });
@@ -32,10 +32,10 @@ class UserController {
                 let passwordDb = result.dataValues.password
                 let compared = Helper.decrypt(password,passwordDb)
                 if(compared){
-                    let {id} = result.dataValues
+                    let {id,name} = result.dataValues
                     let token = Helper.getToken({id,email})
                     req.headers.token = token
-                    res.status(200).json({token})  
+                    res.status(200).json({id,email,name,token})  
                 }else{
                     res.status(400).json({status:400,message:'Email / password wrong'})
                 }
@@ -65,47 +65,40 @@ class UserController {
             //const domain = payload['hd'];
             email = payload.email
             name = payload.name
+
+            User.findOne({
+                where:{
+                    email
+                }
+            })
+            .then((result) => {
+                if(result) {
+                    let {id,name} = result.dataValues
+                    let token = Helper.getToken({id,email})
+                    req.headers.token = token
+                    res.status(200).json({id,email,name,token})
+                }else{
+                    return User.create({
+                        email,
+                        password,
+                        name
+                    })
+                }
+            })
+            .then((result) => {
+                if(result) {
+                    let {id,name} = result.dataValues
+                    let token = Helper.getToken({id,email})
+                    req.headers.token = token
+                    res.status(200).json({id,email,name,token})
+                }
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            });
         }
         verify().catch(console.error);
-
-        User.findOne({
-            where:{
-                email
-            }
-        })
-        .then((result) => {
-            if(result) {
-                let {id} = result.dataValues
-                let token = Helper.getToken({id,email})
-                req.headers.token = token
-                res.status(200).json({token})
-            }else{
-                return User.create({
-                    email,
-                    password,
-                    name
-                })
-            }
-        })
-        .then((result) => {
-            if(result) {
-                let {id} = result.dataValues
-                let token = Helper.getToken({id,email})
-                req.headers.token = token
-                res.status(200).json({token})
-            }
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        });
     }
-
-    // static checkToken(req,res) {
-    //     const jwt = require('jsonwebtoken')
-    //     let decode = jwt.verify(req.headers.token,process.env.SECRET)
-    //     console.log(decode)
-    //     res.status(200).json(decode)
-    // }
 }
 
 module.exports = UserController
